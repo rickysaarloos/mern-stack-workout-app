9172fcecf7c228a35b64aca58ea589aee96cb5b5
rickysaarloos <9027376@student.zadkine.nl>
2026-01-07 12:28:49 +0100

    Opdracht 05 - Models & Schemas

---
+++ workouts/backend/src/models/Workout.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const Schema = mongoose.Schema;
+
+// Schema = regels voor workout
+const workoutSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  reps: {
+    type: Number,
+    required: true
+  },
+  load: {
+    type: Number,
+    required: true
+  }
+}, {
+  timestamps: true
+});
+
+// Model = object voor maken/ophalen/aanpassen/verwijderen
+const Workout = mongoose.model('Workout', workoutSchema);
+
+export default Workout;
--- workouts/backend/src/routes/workoutRoutes.js
+++ workouts/backend/src/routes/workoutRoutes.js
@@ -1,14 +1,42 @@
 // src/routes/workoutRoutes.js
+import Workout from '../models/Workout.js';
 import express from 'express';
+import mongoose from 'mongoose';
 
 // Maak router
 const router = express.Router();
 
 // GET alle workouts
-router.get('/', (req, res) => {
-  res.json({ message: 'GET alle workouts' });
+router.get('/', async (req, res) => {
+  try {
+    const workouts = await Workout.find({}).sort({ createdAt: -1 });
+    res.status(200).json(workouts);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 });
 
+// GET één workout
+router.get('/:id', async (req, res) => {
+  const { id } = req.params;
+
+  // Check of ID geldig is
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: 'Ongeldig workout ID' });
+  }
+
+  try {
+    const workout = await Workout.findById(id);
+
+    if (!workout) {
+      return res.status(404).json({ error: 'Workout niet gevonden' });
+    }
+
+    res.status(200).json(workout);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 // GET één workout
 router.get('/:id', (req, res) => {
   const { id } = req.params;
@@ -16,8 +44,15 @@ router.get('/:id', (req, res) => {
 });
 
 // POST nieuwe workout
-router.post('/', (req, res) => {
-  res.json({ message: 'POST workout', data: req.body });
+router.post('/', async (req, res) => {
+  const { title, load, reps } = req.body;
+
+  try {
+    const workout = await Workout.create({ title, reps, load });
+    res.status(201).json(workout);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // PATCH workout
